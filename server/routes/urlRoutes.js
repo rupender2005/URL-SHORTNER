1f6bcf5d31c1694c222d6f53db65827caf55870f
rupender2005 <163423981+rupender2005@users.noreply.github.com>
2026-09-25 23:41:36 +0530

    Update urlRoutes.js

--- server/routes/urlRoutes.js
+++ server/routes/urlRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const crypto = require("crypto");
-const Url = require("../models/Url");
+const Url = require("../models/url");
 
 const router = express.Router();
 
